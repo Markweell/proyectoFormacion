604784765eabf89be610d9261165298def9cfd73
Marcos Gallardo Pérez <marcos.gallardo@atmira.com>
2019-07-12 09:57:41 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages/pages.component';
+
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { RegistrerComponent } from './login/registrer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: PagesComponent, children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+    ]
+  },
+  {path: 'login', component: LoginComponent},
+  {path: 'registrer', component: RegistrerComponent},
+  {path: '**', component: NopagefoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
